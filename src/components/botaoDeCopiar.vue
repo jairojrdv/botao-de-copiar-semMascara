e06eb1776c81ed00de6870f9a753e15199957516
Jairo Junior <jairo.jr2010@hotmail.com>
2022-12-30 20:02:08 -0300

    First commit

---
+++ src/components/botaoDeCopiar.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <div v-for="user in users" :key="user">
+      <p>{{ user.name }}</p>
+      <p>{{ user.cpf }}</p>
+      <button @click="copy(user.cpf)">Copiar Num</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      cpf: "",
+      users: [
+        {
+          name: "Jairo",
+          cpf: "102.102.102-32",
+        },
+        {
+          name: "Julio",
+          cpf: "321.321./321-36",
+        },
+      ],
+    };
+  },
+  methods: {
+    copy(cpfcnpj) {
+      cpfcnpj = cpfcnpj.replace(/[./-]/g, "");
+      console.log(cpfcnpj);
+      navigator.clipboard.writeText(cpfcnpj);
+    },
+  },
+};
+</script>
+
+<style></style>
